9fff19148622ae7f0bf6f97825c8437f7c89350a
twall <tomwallroth@gmail.com>
2014-03-14 10:29:38 +0100

    tests should not depending on private attributes

--- tinytag/test/test_tinytag.py
+++ tinytag/test/test_tinytag.py
@@ -11,8 +11,8 @@ testfiles = {'vbri.mp3': {'track_total': None, 'length': 0.5224489795918368, 'al
              'UTF16.mp3': {'length': 0.052244897959183675, 'track_total': '11', 'track': '07', 'artist': 'The National', 'year': '2010', 'album': 'High Violet', 'title': 'Lemonworld'},
              'empty.ogg': {'track_total': None, 'length': 3.684716553287982, 'album': None, '_max_samplenum': 162496, 'year': None, 'title': None, 'artist': None, 'track': None, '_tags_parsed': False},
              'multipagecomment.ogg': {'track_total': None, 'length': 3.684716553287982, 'album': None, '_max_samplenum': 162496, 'year': None, 'title': None, 'artist': None, 'track': None, '_tags_parsed': False},
-             'multipage-setup.ogg': {'track_total': None, 'length': 4.128798185941043, 'album': 'Timeless', '_max_samplenum': 182080, 'year': '2006', 'title': 'Burst', 'artist': 'UVERworld', 'track': '7', '_tags_parsed': False},
-             'test.ogg': {'track_total': None, 'length': 1.0, 'album': 'the boss', '_max_samplenum': 44100, 'year': '2006', 'title': 'the boss', 'artist': 'james brown', 'track': '1', '_tags_parsed': False},
+             'multipage-setup.ogg': {'track_total': None, 'length': 4.128798185941043, 'album': 'Timeless', 'year': '2006', 'title': 'Burst', 'artist': 'UVERworld', 'track': '7', '_tags_parsed': False},
+             'test.ogg': {'track_total': None, 'length': 1.0, 'album': 'the boss', 'year': '2006', 'title': 'the boss', 'artist': 'james brown', 'track': '1', '_tags_parsed': False},
              'test.wav': {'length': 1.0},
              'test3sMono.wav': {'length': 3.0},
              'test-tagged.wav': {'length': 1.0},
@@ -32,7 +32,7 @@ def get_info(testfile, expected):
     tag = TinyTag.get(filename)
     for key, value in expected.items():
         result = getattr(tag, key)
-        fmt_string = 'field "%s": %s (%s) is not %s (%s)!'
+        fmt_string = 'field "%s": got %s (%s) expected %s (%s)!'
         fmt_values = (key, repr(result), type(result), repr(value), type(value))
         assert result == value, fmt_string % fmt_values
     print(tag)
