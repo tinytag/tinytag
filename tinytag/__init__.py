f21bc1221e6e2bef7603cec829389963d3384f41
Tom Wallroth <tomwallroth@gmail.com>
2016-06-05 16:05:25 +0200

    bumped version to 0.14.2

--- tinytag/__init__.py
+++ tinytag/__init__.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from .tinytag import TinyTag, TinyTagException, ID3, Ogg, Wave, Flac
+import sys
 
 
-__version__ = '0.14.1'
+__version__ = '0.14.2'
 
 if __name__ == '__main__':
     print(TinyTag.get(sys.argv[1]))
