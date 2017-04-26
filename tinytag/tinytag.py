cb466d80c8d463244ebcdd7ecca513d1eb351fc7
Kojote <re1@a1.net>
2017-04-26 09:01:26 +0200

    Improve value checks on line 519 to avoid ZeroDivisionErrors

--- tinytag/tinytag.py
+++ tinytag/tinytag.py
@@ -516,7 +516,7 @@ class ID3(TinyTag):
                 if xing_header_offset != -1:
                     fh.seek(xing_header_offset, os.SEEK_CUR)
                     xframes, byte_count, toc, vbr_scale = self._parse_xing_header(fh)
-                    if xframes is not None and byte_count is not None:
+                    if xframes and xframes != 0 and ID3.samples_per_frame != 0 and byte_count:
                         self.duration = xframes * ID3.samples_per_frame / float(self.samplerate)
                         self.bitrate = byte_count * 8 / self.duration
                         self.audio_offset = fh.tell()
