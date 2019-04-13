7fac861e32e805b90d7676366c05483a0a98cf5e
Tom Wallroth <tomwallroth@gmail.com>
2019-04-13 15:06:10 +0200

    migrated to use setup.cfg instead of setup.py (thanks to scivision)

--- setup.py
+++ setup.py
@@ -1,65 +1,12 @@
+#!/usr/bin/env python
 from os.path import join
-from setuptools import setup, find_packages
-import sys
-import os
+from setuptools import setup
 
-def read(fname):
-    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
-        return f.read()
 
 def get_version():
-    with open(join('tinytag', '__init__.py')) as f:
-        for line in f:
-            if line.startswith('__version__ ='):
-                return line.split('=')[1].strip().strip('"\'')
+    with open(join("tinytag", "__init__.py")) as f:
+        version_line = next(line for line in f if line.startswith("__version__ ="))
+        return version_line.split("=")[1].strip().strip("\"'")
 
-long_description = None
-if 'upload' in sys.argv or 'register' in sys.argv:
-    readmemd = "\n" + "\n".join([read('README.md')])
-    print("converting markdown to reStucturedText for upload to pypi.")
-    from urllib.request import urlopen
-    from urllib.parse import quote
-    import json
-    import codecs
-    url = 'http://pandoc.org/cgi-bin/trypandoc?from=markdown&to=rst&text=%s'
-    urlhandler = urlopen(url % quote(readmemd))
-    result = json.loads(codecs.decode(urlhandler.read(), 'utf-8'))
-    long_description = result['html']
-else:
-    long_description = "\n" + "\n".join([read('README.md')])
 
-setup(
-    name='tinytag',
-    version=get_version(),
-    description='Read music meta data and length of MP3, OGG, OPUS, MP4, M4A, FLAC, WMA and Wave files',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='Tom Wallroth',
-    author_email='tomwallroth@gmail.com',
-    url='https://github.com/devsnd/tinytag/',
-    license='MIT',
-    packages=find_packages(),
-    install_requires=[],
-    classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'License :: OSI Approved :: MIT License',
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Multimedia',
-        'Topic :: Multimedia :: Sound/Audio',
-        'Topic :: Multimedia :: Sound/Audio :: Analysis',
-    ],
-    zip_safe=False,
-    tests_require=["nose"],
-)
+setup(version=get_version())
