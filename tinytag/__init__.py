5bcfe047e38d53a49eb3bc3ba1ee26a95bc4ef93
Mat <mail@mathias.is>
2024-02-28 02:27:46 +0200

    __init__.py: remove unused code

--- tinytag/__init__.py
+++ tinytag/__init__.py
@@ -1,9 +1,2 @@
-#!/usr/bin/python
 # pylint: disable=missing-module-docstring
-
-import sys
-from .tinytag import TinyTag, TinyTagException  # noqa: F401
-
-
-if __name__ == '__main__':
-    print(TinyTag.get(sys.argv[1]))
+from .tinytag import TinyTag, TinyTagException
